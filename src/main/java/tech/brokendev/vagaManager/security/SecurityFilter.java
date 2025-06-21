da8a48b67b7313cd205248f797dd7a3b57606eac
brokendev <fluccas09@gmail.com>
2025-06-21 06:17:34 -0300

    feat(security): added the new logic to validate token

--- src/main/java/tech/brokendev/vagaManager/security/SecurityFilter.java
+++ src/main/java/tech/brokendev/vagaManager/security/SecurityFilter.java
@@ -4,25 +4,43 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import tech.brokendev.vagaManager.providers.JWTProvider;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
 public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JWTProvider jwtProvider;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
+        SecurityContextHolder.getContext().setAuthentication(null);
+
         String authHeader = request.getHeader("Authorization");
 
-        System.out.println(authHeader);
+        if (authHeader != null) {
+            var subjectToken = this.jwtProvider.validateToken(authHeader);
+            if(subjectToken.isEmpty()){
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                return;
+            }
+            request.setAttribute("company_id", subjectToken);
+            UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(subjectToken, null, Collections.emptyList());
+            SecurityContextHolder.getContext().setAuthentication(auth);
+        }
 
-        throw new UnsupportedOperationException("Unimplemented method  'doFilterInternal'");
-
+        filterChain.doFilter(request, response);
 
     }
 
