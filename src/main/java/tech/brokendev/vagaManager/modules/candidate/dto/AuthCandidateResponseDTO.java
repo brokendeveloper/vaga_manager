1625b01a19efb513a75d700a222e2762cf73da89
brokendev <fluccas09@gmail.com>
2025-06-24 21:24:09 -0300

    feat(security): added auth candidate use case

---
+++ src/main/java/tech/brokendev/vagaManager/modules/candidate/dto/AuthCandidateResponseDTO.java
@@ -0,0 +1,15 @@
+package tech.brokendev.vagaManager.modules.candidate.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthCandidateResponseDTO {
+
+    private String acessToken;
+}
---
+++ src/main/java/tech/brokendev/vagaManager/modules/candidate/useCases/AuthCandidateUseCase.java
@@ -0,0 +1,56 @@
+package tech.brokendev.vagaManager.modules.candidate.useCases;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import tech.brokendev.vagaManager.modules.candidate.CandidateRepository;
+import tech.brokendev.vagaManager.modules.candidate.dto.AuthCandidateRequestDTO;
+import tech.brokendev.vagaManager.modules.candidate.dto.AuthCandidateResponseDTO;
+
+import javax.security.sasl.AuthenticationException;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+
+@Service
+public class AuthCandidateUseCase {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Value("${security.secret.token.candidate}")
+    private String secretKeyCanditate;
+
+    public AuthCandidateResponseDTO execute(AuthCandidateRequestDTO requestDTO) throws AuthenticationException {
+        var candidate = this.candidateRepository.findByUsername(requestDTO.username())
+                .orElseThrow(() -> {
+                    throw new UsernameNotFoundException("username / password incorrect");
+                });
+
+        var passwordMatches = this.passwordEncoder.matches(requestDTO.password(), candidate.getPassword());
+        if (!passwordMatches) {
+            throw new AuthenticationException();
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(secretKeyCanditate);
+        var token = JWT.create()
+                .withIssuer("vagamanager")
+                .withSubject(candidate.getId().toString())
+                .withClaim("roles", Arrays.asList("candidate"))
+                .withExpiresAt(Instant.now().plus(Duration.ofHours(2)))
+                .sign(algorithm);
+
+        var authCandidateResponse = AuthCandidateResponseDTO.builder()
+                .acessToken(token)
+                .build();
+
+        return authCandidateResponse;
+    }
+}
